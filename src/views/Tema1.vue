19384a6fc4bf1840c0eeb1374d6933c04751eecc
Va-Gomez <cmgomez72@misena.edu.co>
2022-09-26 07:46:40 -0500

    actualizando

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,17 +7,113 @@
         span 1
       h1 Fundamentos de posicionamiento SEO
 
+    p.mb-0 El SEO, es un área del marketing digital enfocada a mejorar la presencia de un sitio web, específicamente en la parte natural no pagada. 
+    p.mb-4 <b>A continuación se conocerán detalladamente qué es SEO y en qué consiste el posicionamiento del mismo:</b>
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    .row.justify-content-center.mb-4
+      .col-md-10
+        .cajon.color-acento-botones.p-4(style='background-color: #FFFAE7')
+          p Estas estrategias de marketing digital son fundamentales para que una empresa obtenga reconocimiento en internet y, por lo tanto, obtenga más visitas, clientes y ventas.
+    .row.mb-4
+      .cajon-b.color-secundario.p-3
+        p.mb-0 El SEO es un proceso diseñado por varios componentes que se implementan continuamente. Los resultados no se logran bajo una sola acción, no solamente por utilizar palabras claves, por ejemplo, se puede tener una buena posición en Google. Esto va más allá, se trata de objetivos a largo plazo y una serie de técnicas en grupo, con el fin de mejorar el reconocimiento de un sitio web y atraer visitas relevantes.
+    .row.mb-4
+      .col-md-12.col-12
+        .bloque-texto-g.color-gris.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/tema1/img1.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-5
+            p Cada proyecto o sitio web contiene sus características. No hay un procedimiento SEO que funcione para todas las webs. La estrategia de SEO es diferente para cada sitio web, se requiere ser diseñada una estrategia a cada proyecto web y conlleva tiempo, dedicación y constancia.
     Separador
 
-    #t_1_1.titulo-segundo.color-acento-contenido
+    #t_1_1.titulo-segundo.color-acento-botones
       .h2 1.1	Ventajas del SEO
+
+    p.mb-4 El SEO tiene grandes ventajas para las organizaciones.
+    .row.justify-content-center.align-items-center.mb-4
+      .col-md-4
+        figure
+          img(src='@/assets/curso/tema1/img2.jpg', alt='')
+      .col-md-8
+        LineaTiempoD.color-linea
+          .row.align-items-center(numero="1" titulo="Promover confianza y seguridad") 
+            .col-md-12
+              p Si una página web aparece en los primeros lugares, generará confianza en los usuarios, esto hace que haya más interés y seguridad a la hora de definir el contenido de interés que necesitan. 
+
+          .row.align-items-center(numero="2" titulo="Organizar el sitio web")
+            .col-md-12
+              p Las estrategias de posicionamiento están enfocadas a organizar la forma en que los usuarios interactúan con ella, estas técnicas ayudan a implementar debidamente la información, de su correcta utilización, se logrará posicionar mucho mejor en los motores de búsqueda y también aumentar los resultados del sitio web.
+
+          .row.align-items-center(numero="3" titulo="Procedimiento prolongado")
+            .col-md-12
+              p Los procedimientos e implementación para la optimización de un sitio web requiere horas de trabajo, lograr obtener posicionamiento ayuda a incrementar las visitas de usuarios frecuentemente, sin tener que realizar pautas pagadas.
+
+          .row.align-items-center(numero="4" titulo="Aumenta la presencia") 
+            .col-md-12
+              p Con la aplicación correcta de las técnicas de seo, las visitas aumentarán, así se incrementarán las ventas de un negocio.
     Separador
 
-    #t_1_2.titulo-segundo.color-acento-contenido
+    #t_1_2.titulo-segundo.color-acento-botones
       .h2 1.2	Tipos de SEO
 
+    p.mb-4 El <b> <FONT COLOR="#1565C0"> SEO, </FONT> </b> posiciona un sitio web de forma natural en los motores de búsqueda, sin necesidad de realizar pautas publicitarias pagadas. Estas acciones, actividades y modificaciones para posicionar un sitio web, suelen clasificarse en dos grupos: 
 
+    .row.mb-4
+      .col-md-12
+        TabsC.color-secundario
+          .py-3.py-md-4(titulo="El SEO On Page:")
+            .row.justify-content-center.align-items-center.mb-4
+              .col-md-4.mb-4.mb-md-0
+                figure
+                  img(src='@/assets/curso/tema1/img3.jpg', alt='Texto que describa la imagen')
+                
+              .col-md-6
+                h4 <FONT COLOR="#1565C0"> El SEO On Page: </FONT>
+                p corresponde a las técnicas, actividades y modificaciones que se puede hacer dentro de un sitio web y sus páginas, para posicionarlo en los buscadores. Como por ejemplo organizar su contenido, descripciones, optimización de imágenes, videos, hacen que la duración de carga sea más veloz, todos estos elementos facilitarán al SEO on page, a generar una mejor experiencia con el usuario. En la actualidad Google es el motor de búsqueda más importante
+            
 
+          .py-3.py-md-4(titulo="El SEO Off Page:")
+            .row.justify-content-center.align-items-center.mb-4
+              .col-md-4.mb-4.mb-md-0
+                figure
+                  img(src='@/assets/curso/tema1/img4.jpg', alt='Texto que describa la imagen')
+                
+              .col-md-6
+                h4 <FONT COLOR="#1565C0"> El SEO Off Page: </FONT>
+                p son acciones y actividades que se pueden hacer fuera del sitio web que intentan posicionar mejor en los resultados de búsqueda. Básicamente es suministrar su contenido de forma externa para generar visitas a un sitio web. Normalmente esto se traduce en enlaces entrantes, también conocidos como backlinks, que son enlaces de otros sitios web que dirigen a otra web. Un ejemplo de esto son las redes sociales donde es común encontrar links, enlaces que un sitio web.
+          
+          .py-3.py-md-4(titulo="SEO HAT:")
+            .row.justify-content-center.align-items-center.mb-4
+              .col-md-4.mb-4.mb-md-0
+                figure
+                  img(src='@/assets/curso/tema1/img5.jpg', alt='Texto que describa la imagen')
+                
+              .col-md-6
+                h4 <FONT COLOR="#1565C0"> SEO HAT: </FONT>
+                p corresponde al buen uso de todas las técnicas SEO, sugeridas por los buscadores, implementación, procedimientos que no generen una sanción. Es la claridad, la forma legal de realizar SEO.
+
+          .py-3.py-md-4(titulo="SEO BLACK:")
+            .row.justify-content-center.align-items-center.mb-4
+              .col-md-4.mb-4.mb-md-0
+                figure
+                  img(src='@/assets/curso/tema1/img6.jpg', alt='Texto que describa la imagen')
+                
+              .col-md-6
+                h4 <FONT COLOR="#1565C0"> SEO BLACK: </FONT>
+                p se basa en encontrar un posicionamiento pronto, aplicando elementos, técnicas, poco transparentes, proximidades en las normas de los buscadores, y que conllevarán a sanciones.
+          
+          .py-3.py-md-4(titulo="SEO para Marketing:")
+            .row.justify-content-center.align-items-center.mb-4
+              .col-md-4.mb-4.mb-md-0
+                figure
+                  img(src='@/assets/curso/tema1/img7.jpg', alt='Texto que describa la imagen')
+                
+              .col-md-6
+                h4 <FONT COLOR="#1565C0"> El SEO On Page: </FONT>
+                p Es necesario incorporar la estrategia SEO con las demás actividades de marketing. Se debe tener presente que el marketing tiene sus cimientos en mantener una relación mutuamente beneficiosa con los clientes. Por eso, se debe tener clara la propuesta de marketing, es decir, saber cuál es el valor que se le entrega a un cliente o usuario y qué es lo que se obtiene de ellos a cambio. Una vez se tenga esto definido, se puede iniciar una estrategia SEO.
 
 </template>
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,15 +5,163 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero 
         span 2
-      h1 Fundamentos de posicionamiento SEM 
+      h1 Fundamentos de posicionamiento SEM
+
+    p.mb-4 El SEM son las siglas de Search Engine Marketing, es decir conjunto de técnicas de mercadotecnia que se utiliza para estrategias que nos ayudan a obtener mejor impacto en buscadores generando de esta forma recibir más tráfico en páginas y sitios web a través de un mecanismo de búsqueda. 
+    p.mb-4 <b>Para conocer con mayor precisión en qué consiste el posicionamiento SEM, se podrá ver el siguiente video:</b>
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    .row.mb-4
+      .col-md-12.col-12
+        .bloque-texto-g.color-gris.p-5.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/tema2/img1.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-5.mb-5
+            p <b> <FONT COLOR="#1565C0"> El SEM es el marketing en redes sociales (smm) </FONT> </b> que se busca en la explotación de medios sociales para influir en los usuarios y así persuadirlos hacia la compra de un producto o servicio.
 
     #t_2_1.titulo-segundo.color-acento-contenido
       .h4 2.1	Componentes SEM
-    
+
+    p.mb-4 Diseñar una buena estrategia SEM, implica aspectos a tener en cuenta con el objetivo de obtener los mejores resultados y optimizar la inversión. Se debe diseñar la estrategia teniendo presente los siguientes aspectos:
+
+    .row.justify-content-center.align-items-center.mb-4
+      .tarjeta.color1.p-4
+        .row 
+          .col-md-4
+            figure
+              img(src='@/assets/curso/tema2/img1.png', alt='Texto que describa la imagen')
+          .col-md-8
+            ul.lista-ul--color
+              li 
+                i.fas.fa-check
+                | Una estrategia acorde en la definición de palabras clave.
+              li 
+                i.fas.fa-check
+                | Definir la inversión de la campaña, definición geográfica.
+              li 
+                i.fas.fa-check
+                | Una buena redacción en el anuncio a pautar.
+              li 
+                i.fas.fa-check
+                | Implementación de landing pages que conlleven a la acción. 
+              li 
+                i.fas.fa-check
+                | Estrategias de proporción, la publicidad se maneja por temporalidades esto quiere decir que se debe aprovechar al máximo fechas importantes, definir cómo y cuándo aplicarlas.
+    p.mb-4 <b>Para tener una buena estrategia SEM se deben reconocer las ventajas que tiene:</b>
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-md-5
+        figure
+          img(src='@/assets/curso/tema2/img2.png', alt='Texto que describa la imagen')
+      .col-md-7
+        .row.justify-content-center.align-items-center.mb-4
+          .col-md-10
+            SlyderF.mb-5(columnas="col-lg-12 col-xl-12")
+              .tarjeta-avatar
+                img(src='@/assets/curso/tema2/avatar1.svg' alt='')
+                .tarjeta.color-tarjeta
+                  .text-black.p-4
+                    h5.text-center Crece el tráfico en la web: 
+                    p.mb-0 Realizando un buen trabajo SEM se consigue obtener mejores visitas de nuestra web, teniendo en cuenta que los objetivos pueden ser variados desde ofrecer nuestro producto de hecho hasta potenciar nuestra marca. Por esta razón es importante ajustar las campañas a los objetivos determinados.
+
+              .tarjeta-avatar
+                img(src='@/assets/curso/tema2/avatar2.svg' alt='')
+                .tarjeta.color-tarjeta
+                  .text-black.p-4
+                    h5.text-center Posicionamiento rápido:
+                    p.mb-0 Resultado en menor tiempo, especialmente si lo relacionamos con las técnicas SEO las cuales funcionan a mediano y largo plazo mientras que si se cuenta con el presupuesto en poco tiempo la compañía recibirá vistas de inmediato.
+
+              .tarjeta-avatar
+                img(src='@/assets/curso/tema2/avatar3.svg' alt='')
+                .tarjeta.color-tarjeta
+                  .text-black.p-4
+                    h5.text-center Presupuesto personalizado: 
+                    p.mb-0 Controlar y ajustar el presupuesto y así definir al máximo lo que estamos dispuesto a desembolsar en la compaña. Cuanto se invertirá día tras día y lo que estamos dispuestos a pagar por cada clic (CPC) por lo que se estudiará si realmente conseguimos una oportunidad de venta o conversión.
+
+              .tarjeta-avatar
+                img(src='@/assets/curso/tema2/avatar4.svg' alt='')
+                .tarjeta.color-tarjeta
+                  .text-black.p-4
+                    h5.text-center Poder de segmentación:
+                    p.mb-0 Dependiendo se puede escoger un público objetivo a comparación del SEO. Se puede instaurar en base a unos criterios como edad, comportamiento, palabra clave o localización.
     Separador
 
     #t_2_2.titulo-segundo.color-acento-contenido
       .h4 2.2 Características SEM
+
+    p.mb-4 SEM tiene diferentes tipos de características que permite sea más efectivo su optimización, así mismo los inconvenientes que pueda tener para su implementación. 
+
+    .row.justify-content-center.mb-4
+      .col-md-5.mb-5
+        .tarjetar.color2.p-3.mb-2
+          h4.text-center.mb-0 Características
+        .row.mb-5
+          .col-md-12
+            .tarjeta-numerada.color-acento-contenido.p-4.mb-4
+              .tarjeta-numerada__numero
+                figure
+                  img(src='@/assets/curso/tema2/punto.svg' alt='')
+              h5.mb-1 Acciones de pago
+              p.mb-0 Corresponde al protocolo de pago por click, dicho de otra forma, únicamente se cancela por clicks recibidos por usuarios.
+
+            .tarjeta-numerada.color-acento-contenido.p-4.mb-4
+              .tarjeta-numerada__numero
+                figure
+                  img(src='@/assets/curso/tema2/punto.svg' alt='')
+              h5.mb-1 Posicionamiento rápido
+              p.mb-0 Con la técnica SEM, se obtiene un proceso de posicionamiento más pronto en un tiempo más corto, el objetivo de la pauta paga, es buscar un lugar dentro de las primeras posiciones de Google.
+
+            .tarjeta-numerada.color-acento-contenido.p-4.mb-4
+              .tarjeta-numerada__numero
+                figure
+                  img(src='@/assets/curso/tema2/punto.svg' alt='')
+              h5.mb-1 Utilización de Google Ads
+              p.mb-0 La implementación de la herramienta Google Ads, sirve para realizar campañas SEM, de la manera que se quiera y realizar pagos solamente por los resultados que se hayan obtenido.
+
+            .tarjeta-numerada.color-acento-contenido.p-4.mb-4
+              .tarjeta-numerada__numero
+                figure
+                  img(src='@/assets/curso/tema2/punto.svg' alt='')
+              h5.mb-1 Estrategia de fusión
+              p.mb-0 Diseñar una estrategia de marketing digital, fusionando estrategias de SEM y estrategias de SEO, se logrará obtener posicionamiento a corto, mediano y largo plazo, no se trata de tomar una opción entre las dos, sencillamente las dos son el complemento adecuado para el desarrollo de una buena estrategia de marketing digital.
+
+      .col-md-2
+        span
+    
+      .col-md-5
+        .tarjetar.color4.p-3.mb-2
+          h4.text-center.mb-0 Inconvenientes 
+        .row.mb-5
+          .col-md-12
+            .tarjeta-numerada1.color-acento-contenido.p-4.mb-4
+              .tarjeta-numerada1__numero
+                figure
+                  img(src='@/assets/curso/tema2/punto2.svg' alt='')
+              h5.mb-1 Acciones de pago
+              p.mb-0 Corresponde al protocolo de pago por click, dicho de otra forma, únicamente se cancela por clicks recibidos por usuarios.
+
+            .tarjeta-numerada1.color-acento-contenido.p-4.mb-4
+              .tarjeta-numerada1__numero
+                figure
+                  img(src='@/assets/curso/tema2/punto2.svg' alt='')
+              h5.mb-1 Posicionamiento rápido
+              p.mb-0 Con la técnica SEM, se obtiene un proceso de posicionamiento más pronto en un tiempo más corto, el objetivo de la pauta paga, es buscar un lugar dentro de las primeras posiciones de Google.
+
+            .tarjeta-numerada1.color-acento-contenido.p-4.mb-4
+              .tarjeta-numerada1__numero
+                figure
+                  img(src='@/assets/curso/tema2/punto2.svg' alt='')
+              h5.mb-1 Utilización de Google Ads
+              p.mb-0 La implementación de la herramienta Google Ads, sirve para realizar campañas SEM, de la manera que se quiera y realizar pagos solamente por los resultados que se hayan obtenido.
+
+            .tarjeta-numerada1.color-acento-contenido.p-4.mb-4
+              .tarjeta-numerada1__numero
+                figure
+                  img(src='@/assets/curso/tema2/punto2.svg' alt='')
+              h5.mb-1 Estrategia de fusión
+              p.mb-0 Diseñar una estrategia de marketing digital, fusionando estrategias de SEM y estrategias de SEO, se logrará obtener posicionamiento a corto, mediano y largo plazo, no se trata de tomar una opción entre las dos, sencillamente las dos son el complemento adecuado para el desarrollo de una buena estrategia de marketing digital.
 
     separador
     
